29556c8e74b07ed15df7a42759a8137292302b0d
Suellen <suellen.kathlen@hotmail.com>
2022-05-28 21:45:18 -0300

    first commit

---
+++ EscolhendoComida.py
@@ -0,0 +1,12 @@
+import random
+
+numero = random.randint(1,4)
+
+if numero == 1:
+  print("Salmão com batata")
+elif numero == 2:
+  print ("Pizza")
+elif numero == 3:
+  print ("Sushi")
+elif numero == 4:
+  print ("Feijão com arroz")
